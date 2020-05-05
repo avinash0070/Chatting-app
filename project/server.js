35b50c724685c8349e98a22b6e576564b2b87ecb
avinash0070 <avinashshinde425@gmail.com>
2020-05-05 11:47:38 +0530

    the chatting aap project

---
+++ project/server.js
@@ -0,0 +1,45 @@
+// const app = require('./backend/app.js');
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser'); 
+
+const app = express();
+app.use(express.static(__dirname + '/dist/project'));
+app.use('*',(req,res)=>{
+         res.sendFile(path.join(__dirname))
+        });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+         res.setHeader('Access-Control-Allow-Origin', '*');
+         res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+         res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+        next();
+         });
+              
+const port = process.env.PORT || 3000;
+const server = http.createServer(app)
+var io = require('socket.io').listen(server)
+io.on('connection',(socket)=>{
+           console.log ('io is connected');
+          // for socket on
+          socket.on('join',(data)=>{
+                  socket.join(data.room);
+                    console.log(data.user+' join room'+data.room);
+                  //  TO Brodcast all the user whic room is connected
+                      socket.broadcast.to(data.room).emit('new user joined', {user:data.user, message:'has joined this room.'});
+                               })
+          socket.on('leave', (data)=>{
+                    console.log(data.user + 'left the room : ' + data.room);
+                    
+                     socket.broadcast.to(data.room).emit('left room', {user:data.user, message:'has left this room.'});
+                      socket.leave(data.room);
+                              });
+          socket.on('message',function(data){
+                        io.in(data.room).emit('new message', {user:data.user, message:data.message});
+                        console.log(data)
+                              })
+})
+
+server.listen(port,( )=>console.log('you ARE CONNECTED  :' + port))
